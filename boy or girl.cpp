12bdb9224791b49c1846d4fd2d2362e0ab33fe2c
MorsJeem <105661685+MorsJeem@users.noreply.github.com>
2023-01-30 11:56:33 +0600

    Update boy or girl.cpp

--- boy or girl.cpp
+++ boy or girl.cpp
@@ -2,18 +2,31 @@
 #include<string.h>
 int main()
 {
-	char N1[100];
-	gets(N1);
-	int len=strlen(N1);
-	printf("length %d\n",len);
+	char s[101];
+	gets (s);
+	int len=strlen(s);
+	int i,j;
+	for(i=0;i<len;i++)
+	{
+		for(j=i+1;j<len;j++)
+		{
+			if(s[i]==s[j])
+			{
+				for(j=j;j<len-1;j++)
+				{
+					s[i]=s[j];
+					s[j]=s[j+1];
+				}
+				i--;
+				len--;
+			}
+		}
+	}
 	if(len%2==0)
 	{
-		printf("IGNORE HIM!");
-	}
-	else{
 		printf("CHAT WITH HER!");
 	}
+	else{printf("IGNORE HIM!");
+	}
 	return 0;
-	
-	
 }
